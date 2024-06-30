3872b6129b7b55a31f472511fe080fef4ab20569
Swarnab <swarnabmessibanerjee@gmail.com>
2024-06-30 13:40:01 +0530

    Navbar created

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
+
+export function Navbar({ className }: { className?: string }) {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <MenuItem setActive={setActive} active={active} item="Home"></MenuItem>
+
+        <MenuItem setActive={setActive} active={active} item="Services">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/classes">Class schedule</HoveredLink>
+            <HoveredLink href="/sessions">Private Sessions</HoveredLink>
+            <HoveredLink href="/workshops">Workshops</HoveredLink>
+            <HoveredLink href="/practice">Practice</HoveredLink>
+          </div>
+        </MenuItem>
+
+        <MenuItem
+          setActive={setActive}
+          active={active}
+          item="Contact"
+        ></MenuItem>
+      </Menu>
+    </div>
+  );
+}
